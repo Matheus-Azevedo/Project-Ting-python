f3a05de1bb1a36869d9c5befa1639f278980788d
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-05-07 18:35:47 -0300

    feat: implementado a função capaz de importar notícias a partir de um arquivo TXT, utilizando "\n" como separador

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        print('Formato inválido', file=sys.stderr)
+        return []
+
+    try:
+        with open(path_file, 'r') as f:
+            lines = f.read().split('\n')
+            return lines
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+        return []
